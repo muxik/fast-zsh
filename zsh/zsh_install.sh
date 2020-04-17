c9f2b9a762e143f7e6a305815c488218f8c654fd
muxi_k <lqjxm666@163.com>
2020-04-17 14:35:43 +0800

    zsh 安装配置脚本

---
+++ zsh/zsh_install.sh
@@ -0,0 +1,35 @@
+# /usr/bin/sh
+
+type zsh > /dev/null
+
+if [ $? -ne 0 ] ;then
+    echo "Info: 正在安装zsh"
+    sudo pacman -S zsh > /dev/null
+    if [ $? -ne 0 ];then
+        echo "Error: 安装失败,请检查当前连接"
+    else 
+        echo "Info: zsh安装完成！"
+    fi
+else
+    echo "Info: zsh 已经安装"
+fi
+
+echo "Info: 更换默认用户默认shell"
+chsh -s /bin/zsh > /dev/null
+if [ $? -ne 0 ] ;then 
+    echo "Info: 更改成功！"
+fi
+
+type git > /dev/null
+
+if [ $? -ne 0 ] ;then
+    sudo pacman -S git > /dev/null
+fi
+
+chmod +x ./oh-my-zsh.sh
+echo "Info:正在安装oh-my-zsh,请勿退出..."
+sh -c ./oh-my-zsh.sh > /dev/null
+
+cat ./zshrc > ~/.zshrc 
+echo "Info: 配置完成"; echo "done."
+exec zsh
